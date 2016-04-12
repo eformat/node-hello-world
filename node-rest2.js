c9002f5e5aa736ca36c6caee095c8b53e68938bf
mike hepburn <eformat@gmail.com>
2016-04-12 13:35:10 +1200

    Create node-rest2.js
    new file

---
+++ node-rest2.js
@@ -0,0 +1,14 @@
+var http = require('http');
+for (var item in process.env) 
+console.log(item," = ",process.env[item]);
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("<H1>Node Envvars</H1>");
+  res.write("<table>");    
+for (var item in process.env) 
+  if ((item.substring(0, 3) === "npm")==false)
+  res.write("<TR><TD>"+item+"</TD><TD>"+process.env[item]+"</TD></TR>");
+  res.write("</table>"); 
+  res.end();
+}).listen(8080);
+console.log('JML3  Server  running at http://localhost:8080/');
